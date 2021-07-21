efd74efd071186a34abe4988baa8770ce5ec8bac
cello-wek <falya500@gmail.com>
2021-07-22 00:10:54 +0300

    feat: add reverse function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
-module.exports = function reverse (n) {
-  
+module.exports = function reverse(numb) {
+	console.log(`Number is ${numb}`);
+	const sign = Math.sign(numb);
+	numb = Math.abs(numb);
+	let reversed = '';
+	while (numb) {
+		reversed += String(numb % 10);
+		numb = Math.floor(numb / 10);
+	}
+	console.log(`Reversed number is ${reversed}`);
+	return reversed;
 }
